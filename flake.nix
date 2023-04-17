5b96105cf67b07e6537c0e3131fe8026dbe89826
Maan2003 <manmeetmann2003@gmail.com>
2023-04-17 21:45:21 +0530

    update locks

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "dev environment for carbon lang";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
